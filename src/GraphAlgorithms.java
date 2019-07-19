79a86dd33e3b5e070fcb93bda495b70084b8ad87
Sebastian <jsrt00@hotmail.com>
2019-07-19 13:27:53 -0400

    hw 8

---
+++ src/GraphAlgorithms.java
@@ -0,0 +1,165 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+
+/**
+ * Your implementation of various different graph algorithms.
+ *
+ * @author YOUR NAME HERE
+ * @userid YOUR USER ID HERE (i.e. gburdell3)
+ * @GTID YOUR GT ID HERE (i.e. 900000000)
+ * @version 1.0
+ *
+ * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ *
+ * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ */
+public class GraphAlgorithms {
+
+    /**
+     * Performs a depth first search (dfs) on the input graph, starting at
+     * {@code start} which represents the starting vertex.
+     *
+     * When exploring a vertex, make sure to explore in the order that the
+     * adjacency list returns the neighbors to you. Failure to do so may cause
+     * you to lose points.
+     *
+     * *NOTE* You MUST implement this method recursively, or else you will lose
+     * most if not all points for this method.
+     *
+     * You may import/use {@code java.util.Set}, {@code java.util.List}, and
+     * any classes that implement the aforementioned interfaces, as long as it
+     * is efficient.
+     *
+     * The only instance of {@code java.util.Map} that you may use is the
+     * adjacency list from {@code graph}. DO NOT create new instances of Map
+     * for DFS (storing the adjacency list in a variable is fine).
+     *
+     * DO NOT modify the structure of the graph. The graph should be unmodified
+     * after this method terminates.
+     *
+     * @throws IllegalArgumentException if any input
+     *  is null, or if {@code start} doesn't exist in the graph
+     * @param <T> the generic typing of the data
+     * @param start the vertex to begin the dfs on
+     * @param graph the graph to search through
+     * @return list of vertices in visited order
+     */
+    public static <T> List<Vertex<T>> dfs(Vertex<T> start, Graph<T> graph) {
+        if (start == null) {
+            throw new IllegalArgumentException("Start Vertex is null.");
+        }
+        if (graph == null) {
+            throw new IllegalArgumentException("Graph is null.");
+        }
+        if (!graph.getAdjList().keySet().contains(start)) {
+            throw new IllegalArgumentException("Start not in graph.");
+        }
+        List<Vertex<T>> traversed = new LinkedList<Vertex<T>>();
+        return dfsHelper(start, graph, traversed);
+    }
+
+    private static <T> List<Vertex<T>> dfsHelper(Vertex<T> start,
+                                                 Graph<T> graph,
+                                                 List<Vertex<T>> traversed) {
+        if (!traversed.contains(start)) {
+            traversed.add(start);
+            for (VertexDistance<T> pair: graph.getAdjList().get(start)) {
+                dfsHelper(pair.getVertex(), graph, traversed);
+            }
+        }
+        return traversed;
+    }
+
+
+    /**
+     * Finds the single-source shortest distance between the start vertex and
+     * all vertices given a weighted graph (you may assume non-negative edge
+     * weights).
+     *
+     * Return a map of the shortest distances such that the key of each entry
+     * is a node in the graph and the value for the key is the shortest distance
+     * to that node from {@code start}, or Integer.MAX_VALUE (representing
+     * infinity) if no path exists.
+     *
+     * You may import/use {@code java.util.PriorityQueue},
+     * {@code java.util.Map}, and {@code java.util.Set} and any class that
+     * implements the aforementioned interfaces, as long as your use of it
+     * is efficient as possible.
+     *
+     * You should implement the version of Dijkstra's where you use two
+     * termination conditions in conjunction.
+     *
+     * 1) Check if all of the vertices have been visited.
+     * 2) Check if the PQ is empty yet.
+     *
+     * DO NOT modify the structure of the graph. The graph should be unmodified
+     * after this method terminates.
+     *
+     * @throws IllegalArgumentException if any input is null, or if start
+     *  doesn't exist in the graph.
+     * @param <T> the generic typing of the data
+     * @param start the vertex to begin the Dijkstra's on (source)
+     * @param graph the graph we are applying Dijkstra's to
+     * @return a map of the shortest distances from {@code start} to every
+     *          other node in the graph
+     */
+    public static <T> Map<Vertex<T>, Integer> dijkstras(Vertex<T> start,
+                                                        Graph<T> graph) {
+        if (start == null) {
+            throw new IllegalArgumentException("Start Vertex is null.");
+        }
+        if (graph == null) {
+            throw new IllegalArgumentException("Graph is null.");
+        }
+        if (!graph.getAdjList().keySet().contains(start)) {
+            throw new IllegalArgumentException("Start not in graph.");
+        }
+        Map<Vertex<T>, Integer> map = new HashMap<Vertex<T>, Integer>();
+
+    }
+
+
+    /**
+     * Runs Prim's algorithm on the given graph and returns the Minimum
+     * Spanning Tree (MST) in the form of a set of Edges. If the graph is
+     * disconnected and therefore no valid MST exists, return null.
+     *
+     * You may assume that the passed in graph is undirected. In this framework,
+     * this means that if (u, v, 3) is in the graph, then the opposite edge
+     * (v, u, 3) will also be in the graph, though as a separate Edge object.
+     *
+     * The returned set of edges should form an undirected graph. This means
+     * that every time you add an edge to your return set, you should add the
+     * reverse edge to the set as well. This is for testing purposes. This
+     * reverse edge does not need to be the one from the graph itself; you can
+     * just make a new edge object representing the reverse edge.
+     *
+     * You may assume that there will only be one valid MST that can be formed.
+     *
+     * You should NOT allow self-loops or parallel edges in the MST.
+     *
+     * You may import/use {@code java.util.PriorityQueue},
+     * {@code java.util.Set}, and any class that implements the aforementioned
+     * interface.
+     *
+     * DO NOT modify the structure of the graph. The graph should be unmodified
+     * after this method terminates.
+     *
+     * The only instance of {@code java.util.Map} that you may use is the
+     * adjacency list from {@code graph}. DO NOT create new instances of Map
+     * for this method (storing the adjacency list in a variable is fine).
+     *
+     * @throws IllegalArgumentException if any input
+     *  is null, or if {@code start} doesn't exist in the graph
+     * @param <T> the generic typing of the data
+     * @param start the vertex to begin Prims on
+     * @param graph the graph we are applying Prims to
+     * @return the MST of the graph or null if there is no valid MST
+     */
+    public static <T> Set<Edge<T>> prims(Vertex<T> start, Graph<T> graph) {
+        return null;
+    }
+}
